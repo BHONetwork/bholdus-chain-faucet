f34ed1f371667199915b191c836006aba3610324
Danny Lam <lhdung258@gmail.com>
2021-06-08 11:02:31 +0700

    first commit

---
+++ apps/faucet-telegram-bot/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'faucet-telegram-bot',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/faucet-telegram-bot',
+};
---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/faucet-telegram-bot',
+    '<rootDir>/apps/faucet-discord-bot',
+  ],
+};
