b3dccd7f24e9d3957172d49f70706ffb1210a1ce
Danny Lam <lhdung258@gmail.com>
2021-06-09 16:58:10 +0700

    fix wrong environment key

--- apps/faucet-discord-bot/src/environments/environment.prod.ts
+++ apps/faucet-discord-bot/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const environment = {
   production: true,
   chainUrl: process.env.BHOLDUS_CHAIN_URL,
-  sudoPhrase: process.env.BHOLDUS_PHRASE,
+  sudoPhrase: process.env.BHOLDUS_SUDO_PHRASE,
   sendingAccount: process.env.BHOLDUS_SENDING_ACCOUNT,
   discordKey: process.env.DISCORD_KEY,
 };
